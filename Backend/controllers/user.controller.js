cd262734351f5edfdf808f584fe38fe8bcfd7a51
Adarshjain21 <adarshjain567890@gmail.com>
2024-12-13 23:08:05 +0530

    Add login and logout API

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -2,7 +2,10 @@ import UserModel from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import sendEmail from "../utils/sendEmail.js";
 import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
+import generateAccessToken from "../utils/generateAccessToken.js";
+import generateReferehToken from "../utils/generateRefreshToken.js";
 
+// register controller
 export async function registerUserController(req, res) {
   try {
     const { name, email, password } = req.body;
@@ -37,24 +40,162 @@ export async function registerUserController(req, res) {
     const newUser = new UserModel(payload);
     const save = await newUser.save();
 
-    const verifyEmailUrl = `${process.env.FRONTEND_URL}/verify-email?code=${save?._id}`
+    const verifyEmailUrl = `${process.env.FRONTEND_URL}/verify-email?code=${save?._id}`;
 
     const verifyEmail = await sendEmail({
-        sendTo: email,
-        subject: "Verify email form Quick Commerce",
-        html: verifyEmailTemplate({
-            name,
-            url: verifyEmailUrl
-        })
-    })
+      sendTo: email,
+      subject: "Verify email form Quick Commerce",
+      html: verifyEmailTemplate({
+        name,
+        url: verifyEmailUrl,
+      }),
+    });
 
     return res.json({
       message: "User Registered Successfully",
       error: false,
       success: true,
-      data: save
-    })
+      data: save,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
 
+// verify eamail controller
+export async function verifyEmailController(req, res) {
+  try {
+    const { code } = req.body;
+
+    const user = await UserModel.findOne({ _id: code });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid code",
+        error: true,
+        success: false,
+      });
+    }
+
+    const updateUser = await UserModel.updateOne(
+      { _id: code },
+      {
+        verifyEmail: true,
+      }
+    );
+
+    return res.json({
+      message: "Verify email done",
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+// login controller
+export async function loginController(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Provide email and password",
+        error: true,
+        success: false,
+      });
+    }
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not registered",
+        error: true,
+        success: false,
+      });
+    }
+
+    if (user.status !== "Active") {
+      return res.status(400).json({
+        message: "Contact to Admin",
+        error: true,
+        success: false,
+      });
+    }
+
+    const checkPassword = await bcryptjs.compare(password, user.password);
+
+    if (!checkPassword) {
+      return res.status(400).json({
+        message: "Check your password",
+        error: true,
+        success: false,
+      });
+    }
+
+    const accessToken = await generateAccessToken(user._id);
+    const refreshToken = await generateReferehToken(user._id);
+
+    const cookiesOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    };
+
+    res.cookie("accessToken", accessToken, cookiesOption);
+    res.cookie("refreshToken", refreshToken, cookiesOption);
+
+    return res.json({
+      message: "Login Successfully",
+      error: false,
+      success: true,
+      data: {
+        accessToken,
+        refreshToken,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+// logout controller
+export async function logoutController(req, res) {
+  try {
+    const userid = req.userId;
+
+    const cookiesOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    };
+
+    res.clearCookie("accessToken", cookiesOption);
+    res.clearCookie("refreshToken", cookiesOption);
+
+    const removeRefreshToken = await UserModel.findByIdAndUpdate(userid, {
+      refreshToken: "",
+    });
+
+    return res.json({
+      message: "Logout successfully",
+      error: false,
+      success: true,
+    });
   } catch (error) {
     return res.status(500).json({
       message: error.message || error,
